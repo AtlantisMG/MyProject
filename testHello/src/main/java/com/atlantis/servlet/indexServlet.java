999d5ee5bb79310ef37db5d2c70af30740d0a2c3
Atlantis <1054835728@qq.com>
2019-07-17 16:13:11 +0800

    Master init

---
+++ testHello/src/main/java/com/atlantis/servlet/indexServlet.java
@@ -0,0 +1,42 @@
+package com.atlantis.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/indexServlet")
+public class indexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("接收到请求");
+		PrintWriter writer = response.getWriter();
+		writer.println("success");
+		writer.close();
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
+
+
+
+
+
+
+
+
